5a799349f82f7473b7f1c1809c07c710aa4293d4
Neice <52137719+TrenyceCodes@users.noreply.github.com>
2025-07-18 00:42:55 -0400

    task-cli list commands set up perfectly

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 import os
 import json
 
-currentWorkDirectory = os.getcwd()
-currentFiles = os.listdir(currentWorkDirectory)
-print(currentFiles)
+def getFiles() -> list[str]:
+    currentWorkDirectory = os.getcwd()
+    return os.listdir(currentWorkDirectory)
+
+
+def handleAllListCommands(fileName: str, user_input: str):
+    fileOpen = open(fileName)
+    fileData = json.load(fileOpen)
+
+    for tasks in fileData['tasks']:
+        todo_status = tasks['status']
+
+        match user_input:
+            case 'list':
+                print(tasks)
+            case 'list done':
+                if todo_status == "done":
+                    print(tasks)
+            case 'list todo':
+                if todo_status == "todo":
+                    print(tasks)
+            case 'list in-progress':
+                if todo_status == "in-progress":
+                    print(tasks)
+            case _:
+                print("You entered a command not found, try again")
+
+jsonFile = ""
+currentFiles = getFiles()
 
 for files in currentFiles:
-    print(files)
+    if ".json" in files:
+        jsonFile = str(files)
+   
+
+while jsonFile != "":
+    print("task-cli")
+    user_input = input("\033[1A\033[10C")
+    handleAllListCommands(jsonFile, user_input)
