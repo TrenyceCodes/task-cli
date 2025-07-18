80d452dfbd8207ef071465360985ad4d1c842a07
Neice <52137719+TrenyceCodes@users.noreply.github.com>
2025-07-18 19:35:20 -0400

    Adding hasId function to check for task id in users input

--- main.py
+++ main.py
@@ -5,31 +5,51 @@ def getFiles() -> list[str]:
     currentWorkDirectory = os.getcwd()
     return os.listdir(currentWorkDirectory)
 
+def hasIdNumber(user_input: str) -> int:
+    id = 0
+
+    for number in user_input:
+        if number.isdigit():
+            id = int(number)
+    
+    return id
 
 def handleAllListCommands(fileName: str, user_input: str):
     fileOpen = open(fileName)
     fileData = json.load(fileOpen)
 
     for tasks in fileData['tasks']:
-        todo_status = tasks['status']
+        tasks_status = tasks['status']
 
         match user_input:
             case 'list':
-                print(tasks)
+                print(f'tasks in list: {tasks}')
             case 'list done':
-                if todo_status == "done":
-                    print(tasks)
+                if tasks_status == "done":
+                    print(f'tasks done: {tasks}')
             case 'list todo':
-                if todo_status == "todo":
-                    print(tasks)
+                if tasks_status == "todo":
+                    print(f'tasks todo: {tasks}')
             case 'list in-progress':
-                if todo_status == "in-progress":
-                    print(tasks)
+                if tasks_status == "in-progress":
+                    print(f'In pogress tasks: {tasks}')
             case _:
                 print("You entered a command not found, try again")
 
+def markTodosStatus(fileName: str, taskID: int):
+    fileOpen = open(fileName)
+    fileData = json.load(fileOpen)
+
+    for tasks in fileData['tasks']:
+        tasksID = tasks['id']
+        
+        if tasksID == taskID:
+            print(tasks)
+
+
 jsonFile = ""
 currentFiles = getFiles()
+tasks_list = {}
 
 for files in currentFiles:
     if ".json" in files:
@@ -39,4 +59,9 @@ for files in currentFiles:
 while jsonFile != "":
     print("task-cli")
     user_input = input("\033[1A\033[10C")
-    handleAllListCommands(jsonFile, user_input)
+
+    if "list" in user_input:
+        handleAllListCommands(jsonFile, user_input)
+    elif "mark" in user_input:
+        id = hasIdNumber(user_input)
+        markTodosStatus(jsonFile, id)
